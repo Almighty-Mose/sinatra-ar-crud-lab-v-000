a4067a32ebcbb70ddcdc27e576f7ec6be4fcec0e
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-17 20:23:59 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+
+  POSTS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @content = params[:content]
+  end
+
+  def self.all
+    POSTS
+  end
 end
